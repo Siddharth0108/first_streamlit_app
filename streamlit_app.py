5d5288352aecb8116e88e951a542211013fdd64f
Siddharth0108 <132926855+Siddharth0108@users.noreply.github.com>
2023-05-08 21:35:10 +0530

    Update streamlit_app.py
    slider added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit
 streamlit.title('Pokemon')
 streamlit.header('Pokemon Unite')
 streamlit.text('be a PRO😏like me')
+streamlit.text('Step-1')
+Pokefilter=streamlit.slider('Poke',0,69,42)
